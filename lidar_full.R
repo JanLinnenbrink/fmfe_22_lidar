cd4861ed7b98735f6135f8ac76f98134479259c3
JanLinnenbrink <jan.linnenbrink@web.de>
2022-06-29 17:05:17 +0200

    changes

--- lidar_full.R
+++ lidar_full.R
@@ -14,11 +14,41 @@ opt_stop_early(ctg) <- FALSE
 plot(ctg, chunk=TRUE)
 
 
+## quality checks
+
+las_check(ctg)
+
+
+## classify ground
+ctg <- classify_ground(ctg, algorithm = pmf(ws = 5, th = 3))
+
+
+## calculate the dtm
+opt_output_files(ctg) <- paste0(tempdir(), "{ID}_dtm")
+dtm_tin <- rasterize_terrain(ctg, res = 1, algorithm = tin())
+
+
+## height normalization
+opt_output_files(ctg) <- paste0(tempdir(), "{ID}_nlas")
+nlas <- ctg - dtm
+
+
+## calculate canopy height model
+opt_output_files(ctg) <- paste0(tempdir(), "{ID}_chm")
+chm <- rasterize_canopy(ctg, res = 1, algorithm = p2r())
+
+
 ## calculate tree tops
 opt_output_files(ctg) <- paste0(tempdir(), "{ID}_trees")
-
 ttops <- locate_trees(las, lmf(ws = 5))
 
-#plot(chm, col = height.colors(50))
+
+## c
+
+
+## plots
+plot_dtm3d(dtm_tin, bg = "white")
+plot(nlas, size = 4, bg = "white")
+plot(chm, col = height.colors(50))
 plot(sf::st_geometry(ttops), add = TRUE, pch = 3)
 
