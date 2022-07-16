84ff0dc55827e66cdb4ffe34c7e602bd6454a90b
JanLinnenbrink <jan.linnenbrink@web.de>
2022-07-16 18:06:34 +0200

    shadow calculated, new structure

--- code/lidar_full.R
+++ code/lidar_full.R
@@ -13,7 +13,7 @@ opt_chunk_size(ctg) <- 40
 opt_stop_early(ctg) <- TRUE
 opt_chunk_buffer(ctg) <- 4
 
-plot(classified_ctg, chunk=TRUE)
+plot(ctg, chunk=TRUE)
 
 
 ## quality checks
@@ -27,7 +27,7 @@ classified_ctg <- classify_ground(ctg, algorithm = csf())
 
 #classified_ctg <- readLAScatalog("ground_points")
 
-plot(classified_ctg, color = "Classification", size=3)
+#plot(classified_ctg, color = "Classification", size=3)
 
 ## calculate the dtm
 opt_output_files(classified_ctg) <- paste0(tempdir(), "{ID}_dtm_aktuell")
@@ -38,13 +38,16 @@ dtm_tin <- rasterize_terrain(classified_ctg, res = 1, algorithm = tin())
 
 writeRaster(dtm_tin, "dtm.tif")
 
+#dtm_tin <- rast("dtm.tif")
+
 dtm_prod <- terra::terrain(dtm_tin, v = c("slope", "aspect"), unit = "radians")
 dtm_hillshade <- terra::shade(slope = dtm_prod$slope, aspect = dtm_prod$aspect)
 plot(dtm_hillshade, col = gray(0:50/50), legend = FALSE)
 
 
-## height normalization (not parralel!)
-opt_output_files(classified_ctg) <- paste0(tempdir(), "{ID}_nlas1")
+## height normalization (not in parralel!)
+dir.create(paste0(getwd(), "/nlas"))
+opt_output_files(classified_ctg) <- paste0(getwd(), "/nlas/{ID}_nlas")
 plan(sequential)
 
 ctg_norm <- normalize_height(classified_ctg, dtm_tin)
@@ -57,6 +60,8 @@ opt_output_files(classified_ctg) <- paste0(tempdir(), "{ID}_dhm_new")
 dhm <- rasterize_canopy(classified_ctg, res = 1, algorithm = p2r())
 
 writeRaster(dhm, "dhm.tif")
+#dhm <- rast("dhm.tif")
+
 
 # post processing of dhm
 fill.na <- function(x, i=5) { if (is.na(x)[i]) { return(mean(x, na.rm = TRUE)) } else { return(x[i]) }}
@@ -71,27 +76,41 @@ plot(dhms, col = col)
 ## calculate canopy height model
 
 chm <- rasterize_canopy(ctg_norm, res = 1, algorithm = p2r())
-chm <- dhm - dtm_tin
+#chm <- dhm - dtm_tin
 writeRaster(chm, "chm.tif")
-
+#chm <- rast("chm.tif")
 
 ## segment trees
 
-opt_output_files(ctg_norm) <- ""
-ttops <- locate_trees(ctg_norm, lmf(ws=10), uniqueness = "bitmerge")
-ttops_chm <- locate_trees(chm, lmf(ws=10))
+#ctg_norm@output_options[["drivers"]][["SpatRaster"]][["param"]][["overwrite"]] <- TRUE
 
-st_write(ttops, "ttops.shp")
+opt_filter(ctg_norm) <- "-keep_random_fraction 0.01" # accelelerate processing
+opt_output_files(ctg_norm) <- ""
+opt_chunk_buffer(ctg_norm) <- 4
+#opt_restart(ctg_norm) <- 1
+ttops <- locate_trees(ctg_norm, lmf(ws=40), uniqueness = "bitmerge")
+#ttops_chm <- locate_trees(chm, lmf(ws=10))
+
+ttops <- do.call(rbind,ttops)
+
+st_write(ttops, "ttops.gpkg")
+
+plot(chm, col = height.colors(50))
+plot(sf::st_geometry(ttops), add = TRUE, pch = 3)
 
 # using lidar data
+plan(sequential)
+
 opt_output_files(ctg_norm) <- paste0(tempdir(), "/{*}_segmented")
-algo <- dalponte2016(ctg_norm, ttops)
+algo <- dalponte2016(chm, ttops)
 ctg_segmented <- segment_trees(ctg_norm, algo)
 
-crowns <- crown_metrics(ctg_norm, func = .stdtreemetrics, geom = "convex")
+plan(multisession, workers=availableCores()-2)
+
+crowns <- crown_metrics(ctg_segmented, func = .stdtreemetrics, geom = "convex")
 plot(crowns["convhull_area"], main = "Crown area (convex hull)")
 
-# using the chm only (Without lidar data)
+ # using the chm only (Without lidar data)
 algo <- dalponte2016(chm, ttops)
 crowns <- algo()
 
@@ -99,21 +118,37 @@ plot(crowns, col = pastel.colors(200))
 
 
 ## complex metrices
-ctg <- retrieve_pulses(ctg)
-ctg <- filter_firstctgt(ctg)
-ctg <- filter_poi(ctg, NumberOfReturns > 1)
-ctg@data[, d := Z[1]-Z[2], by = pulseID]
-ctg <- filter_first(ctg)
-ctg <- filter_poi(ctg, !is.na(d))
-plot(ctg, bg = "white", color = "d", colorPalette = viridis::magma(50), size = 4)
+las <- readLAS("clouddc03becdccbf85e0.las", filter = "-keep_random_fraction 0.1")
+las <- retrieve_pulses(las)
+las <- filter_firstlast(las)
+las <- filter_poi(las, NumberOfReturns > 1)
+las@data <- las@data %>% 
+  
+  mutate(d = Z[1]-Z[2])
+
+las@data[, d := Z[1]-Z[2], by = pulseID]
+las <- filter_first(las)
+las <- filter_poi(las, !is.na(d))
+plot(las, bg = "white", color = "d", pal = viridis::magma(50), size = 4)
 # distance to first return
-d_first_ctgt <- pixel_metrics(ctg, ~mean(d), 5)
-plot(d_first_ctgt, col = height.colors(50))
+d_first_last <- pixel_metrics(las, ~mean(d), 5)
+plot(d_first_last, col = height.colors(50))
 # average
 
 ## plots
-plot_dtm3d(dtm_tin, bg = "white")
+plot_dtm3d(dtm_tif, bg = "white")
 plot(nlas, size = 4, bg = "white")
-plot(chm, col = height.colors(50))
+plot(dhm, col = height.colors(50))
 plot(sf::st_geometry(ttops), add = TRUE, pch = 3)
 
+
+
+### shadow calculation (W h m^-2); June 21
+
+shfiles <- list.files("shadows", pattern = "*.tif")
+
+shadows <- rast(paste0("shadows/", shfiles))
+
+shadow <- app(shadows, "sum")
+plot(shadow, col = pals::inferno(50), 
+     plg = list(title = expression(paste('Solar Irradiation', " ", "[Wh", " ",  m^-2,']'))))
